296b92ea454e34e1a0528368a2a00139bab74c7f
Bart Rosa <contact@bartrosa.dev>
2024-05-15 08:47:09 +0200

    Add exercises

---
+++ basics/exercises-1.cpp
@@ -0,0 +1,57 @@
+
+// ###############################################################################
+// # 1. Create a list containing dictionaries that represent information about 
+// # people. Each dictionary should have the keys "name" and "age". Then create a 
+// # function that takes this list and returns a list of names of people who are 
+// # older than 25 years.
+
+
+// ###############################################################################
+// # 2. Create a function that takes two sets of numbers and returns a set 
+// # containing only those numbers that appear in both the first and second sets.
+
+
+// ###############################################################################
+// # 3. Create a nested list that contains lists of numbers. Write a function that
+// # calculates the sum all numbers in this nested list.
+
+
+// ###############################################################################
+// # 4. We have a list of dictionaries representing product information (keys: 
+// # "name", "price", "quantity"). Write a function that generates a dictionary 
+// # from this list, in which the key is the name product, and the value is the 
+// # total price of this product (price * quantity).
+
+
+// ###############################################################################
+// # 5. Create a list of dictionaries representing books (keys: "title", "author",
+// # "year"). Write a function that sorts this list of books alphabetically by 
+// # title.
+
+
+// ###############################################################################
+// # 6. Create a function that takes a list of numbers and returns a set 
+// # containing only unique numbers even numbers from this list.
+
+
+// ###############################################################################
+// # 7. Create a nested list where each item is a list of numbers. Write a 
+// # function that returns list of sums of each nested list.
+
+
+// ###############################################################################
+// # 8. We have a list of dictionaries representing exam results (keys: "name", 
+// # "subject", "score"). Write a function that generates a dictionary from this 
+// # list, in which the keys are items and the values are are the average results 
+// # in a given subject.
+
+// ###############################################################################
+// # 9. Create a function that takes a list of words and returns a set of all the 
+// # letters that appear in those words.
+
+
+// ###############################################################################
+// # 10. Create a nested list that contains dictionaries that represent student 
+// # information (keys: "name", "grades"). Write a function that calculates the 
+// # grade point average for each student i returns a list of dictionaries with 
+// # students' names and their average grades
